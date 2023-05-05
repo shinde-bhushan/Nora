85a0ca840e45e13c6c7682536e069b65d7e1e36c
unknown <piyush.khare8@gmail.com>
2023-05-05 17:04:30 +0530

    added swagger documentation

--- NoraWallet/urls.py
+++ NoraWallet/urls.py
@@ -14,8 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from rest_framework import permissions
+
+# Swagger Schema View Setting
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Nora Wallet API",
+        default_version="v1",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
+    # Swagger Documentation Path
+    path('doc/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+
+    # Django Admin Panel Path
     path('admin/', admin.site.urls),
 ]
